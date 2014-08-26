445f36c98363bf8a2cae5c9f3cb1daa1fc3c2caa
Travis Hairfield <travis.hairfield@opower.com>
2014-08-26 00:36:15 -0400

    Take out debugging print

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ def main():
             reader = csv.reader(f)
             for row in reader:
                 word = row[0].lower()
-                suggestions = t.suggestions(word, max_dist = 3)
-                # out.write("* " + word + ": " + str(suggestions) + "\n")
-                print "* " + word + ": " + str(suggestions)
+                suggestions = t.suggestions(word, max_dist = 2)
+                out.write("* " + word + ": " + str(suggestions) + "\n")
 
 if __name__ == '__main__':
     main()
